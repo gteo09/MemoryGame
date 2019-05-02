9e95b2ee39b8cb43cfd3b35eeb66b4595ede29cc
Gabriel Teotonio <gteo09@gmail.com>
2019-05-01 22:12:39 -0700

    Updates

---
+++ precache-manifest.38a495093c23e27d503e5acce03c29b1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e750a12f1bd2b2fbb9c11a9dc89646a",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "7c3207e102b9b0ecd21c",
+    "url": "/MemoryGame/static/css/main.81c63524.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/MemoryGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "7c3207e102b9b0ecd21c",
+    "url": "/MemoryGame/static/js/main.08938b15.chunk.js"
+  },
+  {
+    "revision": "4c3bd79ed0e125202499",
+    "url": "/MemoryGame/static/js/runtime~main.d54f135b.js"
+  },
+  {
+    "revision": "0345c5ddbb55d98fff4238565742d00e",
+    "url": "/MemoryGame/static/media/burger.0345c5dd.jpg"
+  },
+  {
+    "revision": "953705c8b3d65a51d4d7c3d40958fda1",
+    "url": "/MemoryGame/static/media/cake.953705c8.jpg"
+  },
+  {
+    "revision": "ec395d3427b975c54086af053cb306cf",
+    "url": "/MemoryGame/static/media/donut.ec395d34.jpg"
+  },
+  {
+    "revision": "1f0239dece74c79fc037ad1a5457a6ca",
+    "url": "/MemoryGame/static/media/icecream.1f0239de.jpg"
+  },
+  {
+    "revision": "6b8a30df2e6dfce0debe50096fcc80ec",
+    "url": "/MemoryGame/static/media/pancake.6b8a30df.jpg"
+  },
+  {
+    "revision": "d0b5d9308a2f8e53f9396b465faca7cd",
+    "url": "/MemoryGame/static/media/pasta.d0b5d930.jpg"
+  },
+  {
+    "revision": "c1cbae79157d41aa58463eb690bd2ea0",
+    "url": "/MemoryGame/static/media/pineapple.c1cbae79.jpg"
+  },
+  {
+    "revision": "b891f350df73f1e2d6d02536fe5f9298",
+    "url": "/MemoryGame/static/media/pizza.b891f350.jpg"
+  },
+  {
+    "revision": "3f7522d9a8b07e58a3b0e5211b773a6c",
+    "url": "/MemoryGame/static/media/salad.3f7522d9.jpg"
+  },
+  {
+    "revision": "5d8db3c59e4cf846e00606ec833c770d",
+    "url": "/MemoryGame/static/media/smoothie.5d8db3c5.jpg"
+  },
+  {
+    "revision": "c3f18e27165be0be5abae2eed76fed57",
+    "url": "/MemoryGame/static/media/soup.c3f18e27.jpg"
+  },
+  {
+    "revision": "6dc743a9a76435459cfa7529139b21de",
+    "url": "/MemoryGame/static/media/sushi.6dc743a9.jpg"
+  }
+]);
